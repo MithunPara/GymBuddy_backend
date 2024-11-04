6ba9fa9a84b3cb7c34b00998dbda8588ddd1b1aa
MithunPara <paramatm@mcmaster.ca>
2024-11-04 15:36:04 -0500

    defined the user schema

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -1,4 +1,134 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({}, {timestamps: true});
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    weight: [ // array of weights to track weight changes over time
+        {
+            weight: {
+                type: Number,
+                required: true,
+            },
+            date: {
+                type: Date,
+                required: true,
+            },
+        }
+    ],
+    height: [
+        {
+            height: {
+                type: Number,
+                required: true,
+            },
+            date: {
+                type: Date,
+                required: true,
+            },
+        }
+    ],
+    gender: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: String,
+        required: true,
+    },
+    goal: {
+        type: String,
+        required: true,
+    },
+    calorieIntake: [ 
+        {
+             item: {
+                type: String,
+                required: true,
+             },
+             date: {
+                type: Date,
+                required: true,
+             },
+             amount: {
+                type: Number,
+                required: true,
+             },
+             amountType: {
+                type: String,
+                required: true,
+             },
+             intake: {
+                type: Number,
+                required: true,
+             },
+        }
+    ],
+    activityLevel: {
+        type: String,
+        required: true,
+    },
+    sleep: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            hoursSlept: {
+                type: Number,
+                required: true,
+            },
+        }
+    ],
+    steps: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            steps: {
+                type: Number,
+                required: true,
+            },
+        }
+    ],
+    workouts: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            exercise: {
+                type: String,
+                required: true,
+            },
+            workoutLengthMinutes: {
+                type: Number,
+                required: true,
+            },
+        }
+    ],
+}, {timestamps: true});
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;

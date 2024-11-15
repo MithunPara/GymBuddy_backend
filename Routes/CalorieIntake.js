70b89390f8af38c3cb4731de4a0ab0165a3fbe07
MithunPara <paramatm@mcmaster.ca>
2024-11-15 16:43:20 -0500

    added additional calorie intake routes, retrieving goal calorie intake route in progress

--- Routes/CalorieIntake.js
+++ Routes/CalorieIntake.js
@@ -44,27 +44,84 @@ router.post('/addcalorieintake', authTokenHandler, async (req, res) => {
     headers: {
         'X-Api-Key': process.env.NUTRITION_API_KEY,
     },
-    }, function(error, response, body) {
+    }, async function(error, response, body) {
     if(error) return console.error('Request failed:', error);
     else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-    else console.log(body)
+    else {
+        body = JSON.parse(body);
+        // Amount of calories per gram of the food item * number of grams eaten
+        let calorieIntake = (body[0].calories / body[0].serving_size_g) * parseFloat(amountInGrams);
+        const userId = req.userId;
+        const user = await User.findById({ _id: userId });
+        // update the logged in user's calorie intake
+        user.calorieIntake.push({
+            item,
+            date: new Date(date),
+            amount,
+            amountType,
+            calorieIntake: parseFloat(calorieIntake)
+        });
+
+        await user.save();
+        res.json(createResponse(true, 'Calorie intake updated successfully.'));
+    }
     });
 });
 
 router.post('/getcalorieintakebydate', authTokenHandler, async (req, res) => {
+    const { date } = req.body;
+    const userId = req.userId;
+    const user = await User.findById({ _id: userId });
+    if (!date) {
+        let date = new Date(); // If there is no inputted date, check today's date
+        user.calorieIntake = filterEntriesbyDate(user.calorieIntake, date);
+        return res.json(createResponse(true, 'Calorie intake for today:', user.calorieIntake));
+    }
 
+    user.calorieIntake = filterEntriesbyDate(user.calorieIntake, date);
+    return res.json(createResponse(true, 'Calorie intake for specified date:', user.calorieIntake));
 });
 
 router.post('/getcalorieintakebylimit', authTokenHandler, async (req, res) => {
-
+    const { limit } = req.body;
+    const userId = req.userId;
+    const user = await User.findById({ _id: userId });
+    if(!limit) {
+        return res.status(400).json(createResponse(false, 'Please provide a limit.'));
+    } else if (limit === 'all') {
+        return res.json(createResponse(true, 'Calorie intake:', user.calorieIntake));
+    } else {
+        let date = new Date();
+        date.setDate(date.getDate() - parseInt(limit));
+        user.calorieIntake = filterEntriesbyDate(user.calorieIntake, date);
+        return res.json(createResponse(true, `Calorie intake for last ${limit} days:`, user.calorieIntake));
+    }
 });
 
 router.get('/getgoalcalorieintake', authTokenHandler, async (req, res) => {
+    const userId = req.userId;
+    const user = await User.findById({ _id: userId });
+    let heightInCm = parseFloat(user.height[user.height.length - 1].height);
+    let weightInKg = parseFloat(user.weight[user.weight.length - 1].weight);
+    let age = new Date().getFullYear - new Date(user.dob).getFullYear();
+    
 
 });
 
 router.delete('/deletecalorieintake', authTokenHandler, async (req, res) => {
+    const { item, date } = req.body;
+    const userId = req.userId;
+    const user = await User.findById({ _id: userId});
+    if (!item || !date) {
+        return res.status(400).json(createResponse(false, 'Please provide all the details.'));
+    }
 
+    user.calorieIntake = user.calorieIntake.filter(entry => {
+        return entry.date != date && entry.item != item;
+    });
+
+    await user.save();
+    res.json(createResponse(true, 'Calorie intake deleted successfully.'));
 });
 
 const filterEntriesbyDate = (entries, targetDate) => {
